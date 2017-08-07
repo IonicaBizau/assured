5d02cffbcf32abae244044bca50dc0a477ba5321
Ionică Bizău <bizauionica@gmail.com>
2017-08-07 21:35:46 +0530

    test

--- example/index.js
+++ example/index.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const assured = require("../lib");
+var assured = require("../lib");
 
-let foo = (age, cb) => {
+var foo = function foo(age, cb) {
 
     // Swap the callback function
     if (typeof age === "function") {
@@ -18,33 +18,37 @@ let foo = (age, cb) => {
     }
 
     // Do something async
-    setTimeout(() => {
-        cb(null, `The provided age is ${age}`);
+    setTimeout(function () {
+        cb(null, "The provided age is " + age);
     }, 100);
 
     // Return the promise
     return cb._;
-}
+};
 
 // Callback interface + error
-foo(err => console.log(err));
+foo(function (err) {
+    return console.log(err);
+});
 // => [Error: Invalid age.]
 
 // Callback interface + success
-foo(42, (err, data) => console.log(err, data));
+foo(42, function (err, data) {
+    return console.log(err, data);
+});
 // => null 'The provided age is 42'
 
 // Promise interface + error
-foo().then(x => {
+foo().then(function (x) {
     console.log("Success: ", x);
-}).catch(e => {
+}).catch(function (e) {
     console.log("Error: ", e);
     // => Error:  [Error: Invalid age.]
 });
 
 // Promise + Success
-foo(42).then(x => {
+foo(42).then(function (x) {
     console.log("Success: ", x);
-}).catch(e => {
+}).catch(function (e) {
     console.log("Error: ", e);
 });
